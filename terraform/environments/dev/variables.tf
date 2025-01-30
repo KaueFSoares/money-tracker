270c6097738e7ca9d6cfc8bd4bedb97e986e88fc
Kauê Soares <kaue222120@gmail.com>
2025-01-30 20:53:40 -0300

    fix(terraform): secrets access

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,5 +1,11 @@
-variable "webhook_verify_token" {}
-variable "graph_api_token" {}
+variable "webhook_verify_token" {
+  type      = string
+  sensitive = true
+}
+variable "graph_api_token" {
+  type      = string
+  sensitive = true
+}
 variable "aws_region" {
   default = "us-west-1"
 }

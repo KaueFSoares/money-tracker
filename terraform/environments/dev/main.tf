a25f929fa841baff0109021e9528a4b379d90b50
Kauê Soares <kaue222120@gmail.com>
2025-01-30 07:45:24 -0300

    feat: adds users table

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -7,3 +7,19 @@ module "messages_to_send_queue" {
   source     = "../../modules/sqs"
   queue_name = "messages-to-send-dev"
 }
+
+module "users_table" {
+  source         = "../../modules/dynamodb"
+  table_name     = "users"
+  hash_key       = "userId"
+  attribute_type = "S"
+  billing_mode   = "PROVISIONED"
+
+  global_secondary_indexes = [
+    {
+      name            = "phone-index"
+      hash_key        = "phone"
+      projection_type = "ALL"
+    }
+  ]
+}

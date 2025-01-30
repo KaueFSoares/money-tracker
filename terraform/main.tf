5c843d17f73d7e3a70abb8c1143de507e403b180
Kauê Soares <kaue222120@gmail.com>
2025-01-29 22:55:35 -0300

    fix: removes manually created resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,18 +5,3 @@ provider "aws" {
 terraform {
   required_version = ">= 1.0"
 }
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "money-tracker-terraform-state"
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}

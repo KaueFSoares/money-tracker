a440661da46996ac28bda9a049f0b72c231f22fd
Kauê Soares <kaue222120@gmail.com>
2025-01-30 09:04:13 -0300

    fix(terraform): missing global_secondary_indexes on dynamo db module

--- terraform/modules/dynamo_db/main.tf
+++ terraform/modules/dynamo_db/main.tf
@@ -4,11 +4,23 @@ variable "read_capacity" { default = 5 }
 variable "write_capacity" { default = 5 }
 variable "hash_key" { default = "LockID" }
 variable "attribute_type" { default = "S" }
+variable "global_secondary_indexes" {
+  type    = list(map(string))
+  default = []
+}
 
 resource "aws_dynamodb_table" "this" {
   name           = var.table_name
   billing_mode   = var.billing_mode
   hash_key       = var.hash_key
+
+  global_secondary_indexes = [
+    {
+      name            = "phone-index"
+      hash_key        = "phone"
+      projection_type = "ALL"
+    }
+  ]
 
   dynamic "attribute" {
     for_each = [var.hash_key]

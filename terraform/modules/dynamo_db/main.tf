3a8926a5f9c63f766d9100e625dddd77ddeafeb8
Kauê Soares <kaue222120@gmail.com>
2025-01-30 07:39:20 -0300

    feat: adds dynamo db module

---
+++ terraform/modules/dynamo_db/main.tf
@@ -0,0 +1,36 @@
+variable "table_name" {}
+variable "billing_mode" { default = "PROVISIONED" }
+variable "read_capacity" { default = 5 }
+variable "write_capacity" { default = 5 }
+variable "hash_key" { default = "LockID" }
+variable "attribute_type" { default = "S" }
+
+resource "aws_dynamodb_table" "this" {
+  name           = var.table_name
+  billing_mode   = var.billing_mode
+  hash_key       = var.hash_key
+
+  dynamic "attribute" {
+    for_each = [var.hash_key]
+    content {
+      name = attribute.value
+      type = var.attribute_type
+    }
+  }
+
+  dynamic "provisioned_throughput" {
+    for_each = var.billing_mode == "PROVISIONED" ? [1] : []
+    content {
+      read_capacity  = var.read_capacity
+      write_capacity = var.write_capacity
+    }
+  }
+}
+
+output "table_arn" {
+  value = aws_dynamodb_table.this.arn
+}
+
+output "table_name" {
+  value = aws_dynamodb_table.this.name
+}

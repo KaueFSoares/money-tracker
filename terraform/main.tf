01ccfcc9bfb06b34830d020d7d472eb4437dc833
Kauê Soares <kaue222120@gmail.com>
2025-01-29 22:29:25 -0300

    fix: missing specify terraform backend regions

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,12 +8,14 @@ terraform {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state"
+  region = "us-west-1"
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+  region       = "us-west-1"
 
   attribute {
     name = "LockID"

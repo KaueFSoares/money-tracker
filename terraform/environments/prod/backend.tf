36ebad00d0aa5e8534acd244405ab8f97b5a1e08
Kauê Soares <kaue222120@gmail.com>
2025-01-29 22:35:59 -0300

    refactor: indent

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state"
     key            = "prod/terraform.tfstate"
-    region  = "us-west-1"
+    region         = "us-west-1"
     dynamodb_table = "terraform-lock"
   }
 }

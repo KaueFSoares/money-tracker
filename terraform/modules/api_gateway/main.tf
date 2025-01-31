b6fae757b316f048d2f1d03e53fdde82d6798aa5
Kauê Soares <kaue222120@gmail.com>
2025-01-31 06:38:55 -0300

    fix: missing slash on gateway ep

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -24,7 +24,7 @@ resource "aws_api_gateway_integration" "lambda" {
   http_method = aws_api_gateway_method.proxy_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = var.lambda_arn
+  uri                     = "${var.lambda_arn}/invocations"
 }
 
 output "api_url" {

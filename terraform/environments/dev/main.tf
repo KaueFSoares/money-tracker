7ebb04f27407ff05d86a98d4727051962dba4d93
Kauê Soares <kaue222120@gmail.com>
2025-02-05 21:35:22 -0300

    feat: adds action picker to terraform

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,6 +1,14 @@
+
+// S3
+
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "money-tracker-lambda-bucket"
 }
+
+
+
+
+// SQS
 
 module "messages_received_queue" {
   source     = "../../modules/sqs"
@@ -11,6 +19,11 @@ module "messages_to_send_queue" {
   source     = "../../modules/sqs"
   queue_name = "messages-to-send-dev"
 }
+
+
+
+
+// DYNAMO DB
 
 module "users_table" {
   source         = "../../modules/dynamo_db"
@@ -27,6 +40,11 @@ module "users_table" {
     }
   ]
 }
+
+
+
+
+// LAMBDA - MESSAGE RECEIVER WORKER
 
 resource "aws_iam_role" "message_receiver_worker_role" {
   name = "message-receiver-worker-dev-role"
@@ -83,4 +101,62 @@ module "api_gateway" {
   api_name   = "message-receiver-api-dev"
   lambda_arn = module.message_receiver_worker.function_arn
   aws_region = var.aws_region
+}
+
+
+
+
+// LAMBDA - ACTION PICKER
+
+resource "aws_iam_role" "action_picker_worker_role" {
+  name = "action-picker-worker-dev-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "action_picker_worker_policy" {
+  name        = "action-picker-worker-dev-policy"
+  description = "Allow sending messages to the to send messages queue"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "sqs:SendMessage"
+        Resource = module.messages_to_send_queue.queue_arn
+      },
+      {
+        Effect   = "Allow"
+        Action   = "dynamodb:GetItem"
+        Resource = module.users_table.table_arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "action_picker_worker_attachment" {
+  role       = aws_iam_role.action_picker_worker_role.name
+  policy_arn = aws_iam_policy.action_picker_worker_policy.arn
+}
+
+module "action_picker_worker" {
+  source        = "../../modules/lambda"
+  function_name = "action-picker-worker-dev"
+  handler       = "index.handler"
+  s3_bucket     = aws_s3_bucket.lambda_bucket.id
+  s3_key        = "action-picker-dev.zip"
+  role          = aws_iam_role.action_picker_worker_role.arn
+
+  environment = {
+    SQS_QUEUE_URL        = module.messages_to_send_queue.queue_url
+    REGION               = var.aws_region
+  }
 }

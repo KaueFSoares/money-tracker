338f4fee39ba68031d841fb38f89bc878d247132
Kauê Soares <kaue222120@gmail.com>
2025-02-07 21:08:17 -0300

    fix: queue resources names

--- terraform/environments/dev/queue/messages_received/main.tf
+++ terraform/environments/dev/queue/messages_received/main.tf
@@ -1,4 +1,4 @@
-resource "aws_sqs_queue" "messages_received_dev" {
+resource "aws_sqs_queue" "messages_received_queue" {
   name                       = "messages-received-dev"
   delay_seconds              = 0
   visibility_timeout_seconds = 30
--- terraform/environments/dev/queue/messages_to_send/main.tf
+++ terraform/environments/dev/queue/messages_to_send/main.tf
@@ -1,4 +1,4 @@
-resource "aws_sqs_queue" "messages_to_send" {
+resource "aws_sqs_queue" "messages_to_send_queue" {
   name                       = "messages-to-send-dev"
   delay_seconds              = 0
   visibility_timeout_seconds = 30
